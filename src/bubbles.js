ffb7e5ced47163cb8fbd9c8294b1141ee6d13bf4
Hadri2002 <horvathadri006@gmail.com>
2023-03-17 09:41:07 +0100

    adding bottom bubbles

--- src/bubbles.js
+++ src/bubbles.js
@@ -1,28 +1,37 @@
-let div = document.querySelector(".top");
+let topDiv = document.querySelector(".top");
+let botDiv = document.querySelector(".bottom");
+const darkBlue = "#264A54";
+const midBlue = "#2F6375";
+const lightBlue = "#6FADC3";
+const aqua = "#8ECCB1";
+const white = "#F6F6F2";
 
 let bubbles = [
-    '<svg height="300" width="300"><circle cx="0" cy="148" r="148" fill="#8ECCB1" /></svg>',
-    '<svg height="320" width="400"><circle cx="140" cy="200" r="103" fill="#2F6375" /></svg>',
-    '<svg height="300" width="400"><circle cx="260" cy="100" r="90" fill="#6FADC3" /></svg>',
-    '<svg height="200" width="250"><circle cx="170" cy="60" r="33" fill="#264a54" /></svg>',
-    '<svg height="250" width="350"><circle cx="265" cy="185" r="33" fill="#264a54" /></svg>',
-    '<svg height="300" width="250"><circle cx="210" cy="250" r="12" fill="#F6F6F2" /></svg>',
-    '<svg height="200" width="450"><circle cx="370" cy="120" r="52" fill="#2F6375" /></svg>',
-    '<svg height="300" width="550"><circle cx="440" cy="180" r="70" fill="#264a54" /></svg>',
-    '<svg height="300" width="550"><circle cx="430" cy="60" r="12" fill="#F6F6F2" /></svg>',
-    '<svg height="300" width="600"><circle cx="500" cy="120" r="62" fill="#8ECCB1" /></svg>',
-    '<svg height="300" width="620"><circle cx="570" cy="160" r="37" fill="#6FADC3" /></svg>',
-    '<svg height="300" width="600"><circle cx="540" cy="200" r="16" fill="#F6F6F2" /></svg>',
-    '<svg height="300" width="650"><circle cx="600" cy="185" r="11" fill="#2F6375" /></svg>',
-    '<svg height="300" width="700"><circle cx="650" cy="135" r="29" fill="#2F6375" /></svg>',
-    '<svg height="300" width="720"><circle cx="700" cy="150" r="11" fill="#F6F6F2" /></svg>',
-    '<svg height="300" width="750"><circle cx="720" cy="170" r="20" fill="#6FADC3" /></svg>',
-    '<svg height="300" width="800"><circle cx="760" cy="140" r="18" fill="#8ECCB1" /></svg>',
-    '<svg height="300" width="800"><circle cx="780" cy="160" r="15" fill="#264a54" /></svg>',
-    '<svg height="300" width="850"><circle cx="830" cy="160" r="8" fill="#F6F6F2" /></svg>'
+    [0, 148, 148, aqua], [140, 200, 103, midBlue], [260, 100, 90, lightBlue], [170, 60, 33, darkBlue], [260, 170, 33, darkBlue],
+    [210, 250, 12, white], [370, 120, 52, midBlue], [440, 180, 70, darkBlue], [430, 60, 12, white], [500, 120, 62, aqua], [570, 160, 37, lightBlue],
+    [540, 200, 16, white], [600, 185, 11, midBlue], [650, 135, 29, midBlue], [700, 150, 11, white], [720, 170, 20, lightBlue],
+    [760, 140, 18, aqua], [780, 160, 15, darkBlue], [830, 160, 8, white]
 ]
 
+let number = 0;
 for(var i of bubbles){
-    div.innerHTML += i;
-    div.lastChild.style.position = "absolute";
+    let svg = `<svg height="320" width="850"><circle cx="${i[0]}" cy="${i[1]}" r="${i[2]}" fill="${i[3]}" /></svg>`;
+    topDiv.innerHTML += svg;
+    topDiv.lastChild.style.position = "absolute";
+    topDiv.lastChild.id = "bubble" + number;
+    number++
+}
+
+let svg = '';
+for(var i of bubbles){
+    if(number == 19){
+        svg = `<svg height="320" width="850"><circle cx="${(850-i[0])}" cy="${i[1]}" r="${i[2]}" fill="${darkBlue}" /></svg>`;
+    }
+    else{
+        svg = `<svg height="320" width="850"><circle cx="${(850-i[0])}" cy="${i[1]}" r="${i[2]}" fill="${i[3]}" /></svg>`;
+    }
+    botDiv.innerHTML += svg;
+    botDiv.lastChild.style.position = "absolute";
+    botDiv.lastChild.id = "bubble" + number;
+    number++
 }
